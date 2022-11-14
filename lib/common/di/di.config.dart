c5a383f2499b3a1fc972b5118247245a6a5045a1
Hussain Surti <surti.huss@gmail.com>
2022-11-15 01:20:44 +0400

    added firebase analytics DI

--- lib/common/di/di.config.dart
+++ lib/common/di/di.config.dart
@@ -6,42 +6,53 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:dio/dio.dart' as _i3;
+import 'package:firebase_analytics/firebase_analytics.dart' as _i4;
+import 'package:firebase_core/firebase_core.dart' as _i5;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:logger/logger.dart' as _i4;
+import 'package:logger/logger.dart' as _i6;
 
-import '../../services/apis/peeplEats.dart' as _i5;
-import '../../services/apis/peeplPay.dart' as _i6;
-import '../../services/apis/stripePay.dart' as _i8;
-import '../../utils/log/log_it.dart' as _i9;
-import '../network/services.dart' as _i12;
-import '../router/routes.dart' as _i7;
-import 'dio.dart' as _i10;
-import 'logger_di.dart' as _i11; // ignore_for_file: unnecessary_lambdas
+import '../../services/apis/peeplEats.dart' as _i7;
+import '../../services/apis/peeplPay.dart' as _i8;
+import '../../services/apis/stripePay.dart' as _i10;
+import '../../utils/log/log_it.dart' as _i11;
+import '../network/services.dart' as _i15;
+import '../router/routes.dart' as _i9;
+import 'dio.dart' as _i12;
+import 'firebase.dart' as _i13;
+import 'logger_di.dart' as _i14; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
-_i1.GetIt $initGetIt(_i1.GetIt get,
-    {String? environment, _i2.EnvironmentFilter? environmentFilter}) {
+Future<_i1.GetIt> $initGetIt(_i1.GetIt get,
+    {String? environment, _i2.EnvironmentFilter? environmentFilter}) async {
   final gh = _i2.GetItHelper(get, environment, environmentFilter);
   final dioDi = _$DioDi();
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
   final loggerDi = _$LoggerDi();
   final servicesModule = _$ServicesModule();
   gh.factory<_i3.Dio>(() => dioDi.dio);
-  gh.lazySingleton<_i4.Logger>(() => loggerDi.logger);
-  gh.lazySingleton<_i5.PeeplEatsService>(
-      () => _i5.PeeplEatsService(get<_i3.Dio>()));
-  gh.lazySingleton<_i6.PeeplPaySerivce>(
-      () => _i6.PeeplPaySerivce(get<_i3.Dio>()));
-  gh.singleton<_i7.RootRouter>(servicesModule.rootRouter);
-  gh.lazySingleton<_i8.StripePayService>(
-      () => _i8.StripePayService(get<_i3.Dio>()));
-  gh.lazySingleton<_i9.LogIt>(() => _i9.LogIt(get<_i4.Logger>()));
+  gh.lazySingleton<_i4.FirebaseAnalytics>(
+      () => firebaseInjectableModule.firebaseAnalytics);
+  await gh.factoryAsync<_i5.FirebaseApp>(
+      () => firebaseInjectableModule.firebaseApp,
+      preResolve: true);
+  gh.lazySingleton<_i6.Logger>(() => loggerDi.logger);
+  gh.lazySingleton<_i7.PeeplEatsService>(
+      () => _i7.PeeplEatsService(get<_i3.Dio>()));
+  gh.lazySingleton<_i8.PeeplPaySerivce>(
+      () => _i8.PeeplPaySerivce(get<_i3.Dio>()));
+  gh.singleton<_i9.RootRouter>(servicesModule.rootRouter);
+  gh.lazySingleton<_i10.StripePayService>(
+      () => _i10.StripePayService(get<_i3.Dio>()));
+  gh.lazySingleton<_i11.LogIt>(() => _i11.LogIt(get<_i6.Logger>()));
   return get;
 }
 
-class _$DioDi extends _i10.DioDi {}
+class _$DioDi extends _i12.DioDi {}
 
-class _$LoggerDi extends _i11.LoggerDi {}
+class _$FirebaseInjectableModule extends _i13.FirebaseInjectableModule {}
 
-class _$ServicesModule extends _i12.ServicesModule {}
+class _$LoggerDi extends _i14.LoggerDi {}
+
+class _$ServicesModule extends _i15.ServicesModule {}
--- lib/common/di/firebase.dart
+++ lib/common/di/firebase.dart
@@ -1,18 +1,14 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:firebase_messaging/firebase_messaging.dart';
-// import 'package:injectable/injectable.dart';
-// import 'package:vegan_liverpool/constants/firebaseConfig.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:injectable/injectable.dart';
+import 'package:vegan_liverpool/constants/firebase_options.dart';
 
-// @module
-// abstract class FirebaseInjectableModule {
-//   @lazySingleton
-//   FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+@module
+abstract class FirebaseInjectableModule {
+  @preResolve
+  Future<FirebaseApp> get firebaseApp =>
+      Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
-//   @lazySingleton
-//   FirebaseMessaging get firebaseMessaging => FirebaseMessaging.instance;
-
-//   @preResolve
-//   Future<FirebaseApp> get firebaseApp =>
-//       Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
-// }
+  @lazySingleton
+  FirebaseAnalytics get firebaseAnalytics => FirebaseAnalytics.instance;
+}
